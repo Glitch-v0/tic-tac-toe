a356ac02e5bd70a86f29ac0e595bbcf6c035a0f3
Nathan Jones <njonessurvey@gmail.com>
2023-08-19 21:07:23 -0500

    fix hover not working after 1 match

--- script.js
+++ script.js
@@ -23,25 +23,18 @@ const playerController = (
         var playerTwoTurn = false;
         var canClick = true;
         var cells = document.getElementsByClassName("gridCell");
-        var col1 = [];
-        var col2 = [];
-        var col3 = [];
-        var row1 = [];
-        var row2 = [];
-        var row3 = [];
-        var diag1 = [];
-        var diag2 = [];
-        var winOptions = [];
+        var [col1, col2, col3, row1, row2, row3, diag1, diag2, winOptions] = [[], [], [], [], [], [], [], [], []];
+
         const checkWin = (mark) => {
-            console.log("Checking for win...")
+            //console.log("Checking for win...")
             const resetCells = (message) => {
                 canClick = false;
                     setTimeout(() => {
                         alert(`${message}`);
-                        for(cell of cells){
+                        for(let cell of cells){
                             cell.textContent = "";
                             cell.style.backgroundColor = "white";
-                            cell.style.fontSize = "clamp(2rem, 5vw, 3.5rem)"
+                            cell.style.fontSize = "clamp(2.5rem, 6vw, 3.5rem)";
                         }
                         canClick = true;
                     }, 400);
@@ -51,60 +44,32 @@ const playerController = (
                     option.forEach(cell => {
                         cell.style.backgroundColor = "var(--flash-color)"; // Highlight the winning cells
                     });
-                    console.log("Player with", mark, "has won on", option[0].classList);
+                    //console.log("Player with", mark, "has won on", option[0].classList);
                     canClick = false;
                     resetCells("Player " + (mark == "X" ? "One" : "Two") + " Wins! Player " + (mark == "X" ? "Two" : "One") + " starts now.")
                     break
                 } else {
-                    console.log('No win yet.')
+                    //console.log('No win yet.')
                 }
             }
             if (Array.from(cells).every(cell => cell.textContent != "")){
                 resetCells("It's a draw! Player " + (playerOneTurn == true ? "Two" : "One") + " starts the next round.");
             } 
             }
-        for (let i = 0; i < cells.length; i++) {
-            //Add row win area
-            if(cells[i].id.includes("A")){
-                row1.push(cells[i])
-                console.log(`Added ${cells[i].id} to row1 ${row1}`)
+        const cellIteration = (character, cell, grouptoPush) => {
+            if(cell.id.includes(character)){
+                grouptoPush.push(cell)
+                //console.log(`Added ${cell.id} to ${grouptoPush}`)
+            }}
+        // Iterates through placing the cells in various groupings for win conditions
+        const itterArray = [["A", row1], ["B", row2], ["C", row3], ["1", col1], ["2", col2], ["3", col3],
+        ["A1", diag1], ["B2", diag1], ["C3", diag1], ["C1", diag2], ["B2", diag2], ["A3", diag2]]
+        for (let cell of cells) {
+            for (let [character, group] of itterArray) {
+                cellIteration(character, cell, group);
             }
-            if(cells[i].id.includes("B")){
-                row2.push(cells[i])
-                console.log(`Added ${cells[i].id} to row1 ${row2}`)
-            }
-            if(cells[i].id.includes("C")){
-                row3.push(cells[i])
-                console.log(`Added ${cells[i].id} to row1 ${row3}`)
-            }
-            //Add column win area
-            if(cells[i].id.includes("1")){
-                col1.push(cells[i])
-                console.log(`Added ${cells[i].id} to row1 ${col1}`)
-            }
-            if(cells[i].id.includes("2")){
-                col2.push(cells[i])
-                console.log(`Added ${cells[i].id} to row1 ${col2}`)
-            }
-            if(cells[i].id.includes("3")){
-                col3.push(cells[i])
-                console.log(`Added ${cells[i].id} to row1 ${col3}`)
-            }
-            //Add diagonal win area
-            if(cells[i].id.includes("A1") || cells[i].id.includes("B2") || cells[i].id.includes("C3")){
-                diag1.push(cells[i])
-                console.log(`Added ${cells[i].id} to row1 ${diag1}`)
-            }
-            if(cells[i].id.includes("C1") || cells[i].id.includes("B2") || cells[i].id.includes("A3")){
-                diag2.push(cells[i])
-                console.log(`Added ${cells[i].id} to row1 ${diag2}`)
-            }
-            winOptions.push(row1, row2, row3, col1, col2, col3, diag1, diag2)
-            console.log({row1, row2, row3, col1, col2, col3, diag1, diag2})
-            console.log({winOptions})
             //Turn handling
-            cells[i].addEventListener("mousedown", (e) => {
-                var cell = cells[i];
+            cell.addEventListener("mousedown", (e) => {
                 if (cell.textContent != "" || canClick == false) {
                     // Do nothing
                 } else {
@@ -130,5 +95,8 @@ const playerController = (
                 }
             })
         }
-        }
-)()
+        //Adds various cell groups to win conditions
+        winOptions.push(row1, row2, row3, col1, col2, col3, diag1, diag2)
+        //console.log({row1, row2, row3, col1, col2, col3, diag1, diag2})
+        //console.log({winOptions})
+    })()
